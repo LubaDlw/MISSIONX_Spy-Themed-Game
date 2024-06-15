36bc5c23660fd94b7a6f5108f93b9ad2b17e90dc
LubaDlw <132472444+LubaDlw@users.noreply.github.com>
2024-06-15 14:57:51 +0200

    SIlhouetteLogic

--- Assets/Scripts/CriminalInfo.cs
+++ Assets/Scripts/CriminalInfo.cs
@@ -9,10 +9,30 @@ public class CriminalInfo : MonoBehaviour
     public int TotalClues;
     public int cluesFound;
     public int points;
+    public GameObject Silhouette;
+    public GameObject ColorImage;
 
     public CriminalInfo(string name, List<GameObject> blackSlots)
     {
         this.name = name;
         this.blackSlots = blackSlots;
+    }
+
+    void Start()
+    {
+        // At the beginning of the game, set the silhouette to active and color image to inactive
+        Silhouette.SetActive(true);
+        ColorImage.SetActive(false);
+    }
+
+    void Update()
+    {
+        // Check if all clues have been found
+        if (cluesFound >= TotalClues)
+        {
+            // Switch the active states of the silhouette and color image
+            Silhouette.SetActive(false);
+            ColorImage.SetActive(true);
+        }
     }
 }
